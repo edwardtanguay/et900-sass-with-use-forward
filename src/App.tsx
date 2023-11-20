721f0d9d7486a4afed1ad7ac87663d3bcae3f68b
Edward Tanguay (windows) <edward@tanguay.info>
2023-11-20 03:41:05 +0100

    changed titles

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import './sass/main.scss';
 function App() {
 	return (
 		<>
-		<h1>Sass with @import</h1>
+		<h1>Sass with @use/@forward</h1>
 		<p className="error-text spacing">This is an error.</p>
 		<p className="info-text spacing">This is an info.</p>
 		<p className="normal-text spacing">This is a normal text.</p>
